321a607823569f82220e3009247aa024bb35b13b
devchlee12 <devchlee12@gmail.com>
2024-07-05 23:58:08 +0900

    [FEAT] 프로젝트 시작

---
+++ src/main/java/com/freshman/freshmanbackend/FreshmanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.freshman.freshmanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshmanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshmanBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freshman/freshmanbackend/FreshmanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshman.freshmanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshmanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
