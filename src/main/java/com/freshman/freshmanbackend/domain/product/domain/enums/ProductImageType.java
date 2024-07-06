f41544a86199cbe5474c09a8c7fb8cf1d9aa6be1
ByungSun Song <46879319+SBSun@users.noreply.github.com>
2024-07-06 17:35:09 +0900

    [FEAT] 상품 관련 엔티티 및 Enum Converter 추가 (#3)

---
+++ src/main/java/com/freshman/freshmanbackend/domain/product/domain/enums/ProductImageType.java
@@ -0,0 +1,49 @@
+package com.freshman.freshmanbackend.domain.product.domain.enums;
+
+import com.freshman.freshmanbackend.global.common.domain.converter.AbstractEnumCodeConverter;
+import com.freshman.freshmanbackend.global.common.domain.enums.Codable;
+
+import java.util.EnumSet;
+
+import jakarta.persistence.Converter;
+import lombok.Getter;
+
+/**
+ * 상품 이미지 타입
+ *
+ * @author 송병선
+ */
+@Getter
+public enum ProductImageType implements Codable {
+
+  /**
+   * 목록 이미지
+   */
+  LIST("L"),
+  /**
+   * 상세 이미지
+   */
+  DETAIL("D");
+
+  private final String code;
+
+  ProductImageType(String code) {
+    this.code = code;
+  }
+
+  public static boolean containCode(String code) {
+    return EnumSet.allOf(ProductImageType.class).stream().anyMatch(e -> e.getCode().equals(code));
+  }
+
+  public static ProductImageType fromCode(final String code) {
+    return Codable.fromCode(ProductImageType.class, code);
+  }
+
+  @Converter
+  public static class TypeCodeConverter extends AbstractEnumCodeConverter<ProductImageType> {
+    @Override
+    public ProductImageType convertToEntityAttribute(String dbData) {
+      return this.toEntityAttribute(ProductImageType.class, dbData);
+    }
+  }
+}
---
+++ src/main/java/com/freshman/freshmanbackend/global/common/domain/enums/Valid.java
@@ -0,0 +1,42 @@
+package com.freshman.freshmanbackend.global.common.domain.enums;
+
+import com.freshman.freshmanbackend.global.common.domain.converter.AbstractEnumCodeConverter;
+
+import java.util.EnumSet;
+
+import jakarta.persistence.Converter;
+import lombok.Getter;
+
+/**
+ * 유효여부
+ *
+ * @author 송병선
+ */
+@Getter
+public enum Valid implements Codable {
+  
+  TRUE("Y"),
+  FALSE("N");
+
+  private final String code;
+
+  Valid(String code) {
+    this.code = code;
+  }
+
+  public static boolean containCode(String code) {
+    return EnumSet.allOf(Valid.class).stream().anyMatch(e -> e.getCode().equals(code));
+  }
+
+  public static Valid fromCode(final String code) {
+    return Codable.fromCode(Valid.class, code);
+  }
+
+  @Converter
+  public static class TypeCodeConverter extends AbstractEnumCodeConverter<Valid> {
+    @Override
+    public Valid convertToEntityAttribute(String dbData) {
+      return this.toEntityAttribute(Valid.class, dbData);
+    }
+  }
+}
