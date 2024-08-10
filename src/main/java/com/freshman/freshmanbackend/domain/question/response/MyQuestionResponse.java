740ca96ffaa440d88ae1140b8d4b63cd10d3c7dd
devchlee12 <devchlee12@gmail.com>
2024-08-10 23:36:00 +0900

    [FEAT] 문의 응답 객체 정적팩토리 메소드 추가

--- src/main/java/com/freshman/freshmanbackend/domain/question/response/MyQuestionResponse.java
+++ src/main/java/com/freshman/freshmanbackend/domain/question/response/MyQuestionResponse.java
@@ -1,7 +1,12 @@
 package com.freshman.freshmanbackend.domain.question.response;
 
+import com.freshman.freshmanbackend.domain.question.domain.Question;
+import com.freshman.freshmanbackend.global.auth.util.AuthMemberUtils;
+
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,7 +17,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
 public class MyQuestionResponse {
+  private Long questionSeq;
   private String memberName;
   private String content;
   private String image;
@@ -21,4 +29,17 @@ public class MyQuestionResponse {
   private String productName;
   private String productImage;
   private Long productSeq;
+
+  public static MyQuestionResponse of(Question question) {
+    return MyQuestionResponse.builder()
+                             .questionSeq(question.getQuestionSeq())
+                             .memberName(AuthMemberUtils.getOauthUserDto().getName())
+                             .content(question.getContent())
+                             .image(question.getImage())
+                             .postedDate(question.getCreatedAt().toLocalDate())
+                             .isAnswered(question.getIsAnswered())
+                             .productName(question.getProduct().getName())
+                             .productSeq(question.getProduct().getProductSeq())
+                             .build();
+  }
 }
