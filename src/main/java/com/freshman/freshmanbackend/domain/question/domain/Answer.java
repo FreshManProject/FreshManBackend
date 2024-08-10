82061713dba8608657771e798a4c072ca80080d6
devchlee12 <devchlee12@gmail.com>
2024-08-10 23:40:06 +0900

    [FIX] 답변 엔티티 수정

--- src/main/java/com/freshman/freshmanbackend/domain/question/domain/Answer.java
+++ src/main/java/com/freshman/freshmanbackend/domain/question/domain/Answer.java
@@ -4,30 +4,30 @@ import com.freshman.freshmanbackend.global.common.domain.BaseTimeEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 /**
  * 답변 엔티티
  */
 @Entity
+@NoArgsConstructor
+@Getter
+@ToString
 public class Answer extends BaseTimeEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "ANS_SEQ", nullable = false)
   private Long answerSeq;
 
-  @Column(name = "ANS_TITLE", nullable = false)
-  private String title;
-
   @Column(name = "ANS_CONT", nullable = false)
   private String content;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "QST_SEQ")
-  private Question question;
+  public Answer(String content) {
+    this.content = content;
+  }
 }
