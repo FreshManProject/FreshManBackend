9208f819e4e46bf847795b9c9c7d96ba25a30467
devchlee12 <devchlee12@gmail.com>
2024-08-10 23:39:48 +0900

    [FEAT] 답변 컨트롤러 추가

---
+++ src/main/java/com/freshman/freshmanbackend/domain/question/controller/AnswerController.java
@@ -0,0 +1,41 @@
+package com.freshman.freshmanbackend.domain.question.controller;
+
+import com.freshman.freshmanbackend.domain.question.controller.validator.AnswerValidator;
+import com.freshman.freshmanbackend.domain.question.request.AnswerEntryRequest;
+import com.freshman.freshmanbackend.domain.question.response.AnswerResponse;
+import com.freshman.freshmanbackend.domain.question.service.AnswerService;
+import com.freshman.freshmanbackend.global.common.response.SuccessResponse;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 답변 컨트롤러
+ */
+@RestController
+@RequestMapping("/answers")
+@RequiredArgsConstructor
+public class AnswerController {
+  private final AnswerService answerService;
+
+  @GetMapping("/{questionSeq}")
+  public ResponseEntity<?> get(@PathVariable(name = "questionSeq") Long questionSeq) {
+    AnswerResponse answer = answerService.getByQuestionSeq(questionSeq);
+    return ResponseEntity.ok(answer);
+  }
+
+  @PostMapping("/{questionSeq}")
+  public ResponseEntity<?> post(@PathVariable(name = "questionSeq") Long questionSeq,
+      @RequestBody AnswerEntryRequest answerEntryRequest) {
+    AnswerValidator.validate(answerEntryRequest);
+    answerService.save(answerEntryRequest, questionSeq);
+    return ResponseEntity.ok(new SuccessResponse());
+  }
+}
