0112b78210642fc0ae786a71bf00f94fee18584e
devchlee12 <devchlee12@gmail.com>
2024-08-10 23:34:44 +0900

    [FIX] 문의 응답 객체를 생성할 때 정적 팩토리 메소드를 사용하도록 변경

--- src/main/java/com/freshman/freshmanbackend/domain/question/response/ProductQuestionResponse.java
+++ src/main/java/com/freshman/freshmanbackend/domain/question/response/ProductQuestionResponse.java
@@ -1,8 +1,12 @@
 package com.freshman.freshmanbackend.domain.question.response;
 
+import com.freshman.freshmanbackend.domain.question.domain.Question;
+import com.freshman.freshmanbackend.global.auth.util.AuthMemberUtils;
+
 import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,10 +18,23 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class ProductQuestionResponse {
+  private Long questionSeq;
   private String memberName;
   private String content;
   private String image;
   private Boolean isAnswered;
   private LocalDate postedDate;
+
+  public static ProductQuestionResponse fromQuestion(Question question) {
+    return ProductQuestionResponse.builder()
+                                  .questionSeq(question.getQuestionSeq())
+                                  .memberName(AuthMemberUtils.getOauthUserDto().getName())
+                                  .content(question.getContent())
+                                  .image(question.getImage())
+                                  .isAnswered(question.getIsAnswered())
+                                  .postedDate(question.getCreatedAt().toLocalDate())
+                                  .build();
+  }
 }
