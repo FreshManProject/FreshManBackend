843a2137b8b08398bc3e758c3636179e24a85be3
Chanho lee <devchlee12@gmail.com>
2024-09-21 00:00:00 +0900

    [FEAT] 리뷰 응답 객체 추가

---
+++ src/main/java/com/freshman/freshmanbackend/domain/product/response/ProductReviewResponse.java
@@ -0,0 +1,23 @@
+package com.freshman.freshmanbackend.domain.product.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.freshman.freshmanbackend.domain.product.domain.Review;
+import com.freshman.freshmanbackend.domain.product.repository.ReviewRepository;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ProductReviewResponse {
+    private Long reviewSeq;
+    private String userName;
+    private String content;
+    private Byte score;
+    private String imagePath;
+    private LocalDate createdAt;
+}
