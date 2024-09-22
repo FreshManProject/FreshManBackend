cede690028f60a6fc3505f4da38e3ba4d037f5a5
Chanho lee <devchlee12@gmail.com>
2024-09-22 17:31:44 +0900

    [FEAT] 멤버 개인정보, 주소 업데이트 기능 추가

--- src/main/java/com/freshman/freshmanbackend/domain/member/service/MemberService.java
+++ src/main/java/com/freshman/freshmanbackend/domain/member/service/MemberService.java
@@ -2,6 +2,8 @@ package com.freshman.freshmanbackend.domain.member.service;
 
 import com.freshman.freshmanbackend.domain.member.domain.Member;
 import com.freshman.freshmanbackend.domain.member.repository.MemberRepository;
+import com.freshman.freshmanbackend.domain.member.request.MemberAddressUpdateRequest;
+import com.freshman.freshmanbackend.domain.member.request.MemberInfoUpdateRequest;
 import com.freshman.freshmanbackend.domain.member.request.MemberUpdateRequest;
 import com.freshman.freshmanbackend.domain.member.response.MemberResponse;
 import com.freshman.freshmanbackend.global.auth.util.AuthMemberUtils;
@@ -44,5 +46,27 @@ public class MemberService {
     String oauth2Id = AuthMemberUtils.getUserOauth2Id();
     Member member = memberRepository.findByOauth2Id(oauth2Id);
     member.updateMember(request);
+  }
+
+  /**
+   * 회원 개인 정보 업데이트
+   * @param request 회원 개인정보
+   */
+  @Transactional
+  public void updateInfo(MemberInfoUpdateRequest request){
+    String userOauth2Id = AuthMemberUtils.getUserOauth2Id();
+    Member member = memberRepository.findByOauth2Id(userOauth2Id);
+    member.updateInfo(request);
+  }
+
+  /**
+   * 회원 주소 업데이트
+   * @param request 회원 주소
+   */
+  @Transactional
+  public void updateAddress(MemberAddressUpdateRequest request){
+    String userOauth2Id = AuthMemberUtils.getUserOauth2Id();
+    Member member = memberRepository.findByOauth2Id(userOauth2Id);
+    member.updateAddress(request);
   }
 }
