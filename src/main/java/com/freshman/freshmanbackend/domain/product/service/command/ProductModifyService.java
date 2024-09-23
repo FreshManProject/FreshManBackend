456a52b87850b31f20d8cd442f4398470e88e77d
Chanho lee <devchlee12@gmail.com>
2024-09-23 19:15:41 +0900

    [FEAT] 상품 수정 기능에 이미지 추가

--- src/main/java/com/freshman/freshmanbackend/domain/product/service/command/ProductModifyService.java
+++ src/main/java/com/freshman/freshmanbackend/domain/product/service/command/ProductModifyService.java
@@ -2,11 +2,13 @@ package com.freshman.freshmanbackend.domain.product.service.command;
 
 import com.freshman.freshmanbackend.domain.product.domain.Product;
 import com.freshman.freshmanbackend.domain.product.domain.ProductCategory;
+import com.freshman.freshmanbackend.domain.product.domain.ProductImage;
 import com.freshman.freshmanbackend.domain.product.domain.ProductSale;
 import com.freshman.freshmanbackend.domain.product.request.ProductModifyRequest;
 import com.freshman.freshmanbackend.domain.product.request.ProductSaleRequest;
 import com.freshman.freshmanbackend.domain.product.service.query.ProductCategoryOneService;
 import com.freshman.freshmanbackend.domain.product.service.query.ProductOneService;
+import com.freshman.freshmanbackend.global.cloud.service.S3UploadService;
 import com.freshman.freshmanbackend.global.common.exception.ValidationException;
 import com.freshman.freshmanbackend.global.common.utils.DateTimeUtils;
 
@@ -14,6 +16,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 상품 수정 서비스
@@ -23,9 +30,11 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ProductModifyService {
-
+  private static final String THUMBNAIL_FOLDER = "thumbnail/";
+  private static final String MAIN_IMAGE_FOLDER = "main-images/";
   private final ProductOneService productOneService;
   private final ProductCategoryOneService productCategoryOneService;
+  private final S3UploadService s3UploadService;
 
   /**
    * 상품 수정
@@ -34,12 +43,45 @@ public class ProductModifyService {
    */
   @Transactional
   public void modify(ProductModifyRequest param) {
+    String thumbnailPath;
+    int order = 0;
+    List<ProductImage> productImages = new ArrayList<>();
+
     // 상품 및 카테고리 조회
     Product product = productOneService.getOne(param.getProductSeq(), Boolean.TRUE);
     ProductCategory category = productCategoryOneService.getOne(param.getCategorySeq());
 
     // 상품 정보 수정
     product.update(param.getName(), param.getPrice(), param.getDescription(), param.getBrand(), category);
+
+    //기존 메인 이미지 삭제
+    s3UploadService.deleteAllFilesUnderPath(MAIN_IMAGE_FOLDER + product.getProductSeq() + "/");
+
+    //이미지 변경
+    MultipartFile thumbnailImage = param.getThumbnailImage();
+    List<MultipartFile> mainImages = param.getMainImages();
+    
+    //썸네일 이미지 업로드
+    try {
+      thumbnailPath = s3UploadService.saveFile(thumbnailImage, THUMBNAIL_FOLDER + product.getProductSeq());
+      product.setThumbnailImage(thumbnailPath);
+    } catch (IOException e) {
+      throw new ValidationException("s3.save_failed");
+    }
+    
+    //메인 이미지 리스트 업로드
+    for (MultipartFile file : mainImages){
+      try {
+        String fileUrl = s3UploadService.saveFile(file, MAIN_IMAGE_FOLDER + product.getProductSeq() + "/" + order);
+        productImages.add(new ProductImage(fileUrl, order));
+        order++;
+      } catch (IOException e) {
+        throw new ValidationException("s3.save_failed");
+      }
+    }
+    
+    //메인 이미지 업데이트
+    product.updateImageList(productImages);
   }
 
   /**
