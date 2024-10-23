23498a46900e42457c39a55f9879f2f5225395a3
Chanho lee <devchlee12@gmail.com>
2024-10-23 16:59:19 +0900

    [FEAT] 상품 조회시 좋아요 누름 여부 반환 기능 구현

--- src/main/java/com/freshman/freshmanbackend/domain/product/dao/ProductListDao.java
+++ src/main/java/com/freshman/freshmanbackend/domain/product/dao/ProductListDao.java
@@ -1,6 +1,7 @@
 package com.freshman.freshmanbackend.domain.product.dao;
 
 import com.freshman.freshmanbackend.domain.product.domain.QProduct;
+import com.freshman.freshmanbackend.domain.product.domain.QProductLike;
 import com.freshman.freshmanbackend.domain.product.domain.QProductSale;
 import com.freshman.freshmanbackend.domain.product.domain.enums.ProductSortType;
 import com.freshman.freshmanbackend.domain.product.request.ProductListRequest;
@@ -11,14 +12,11 @@ import com.querydsl.core.types.ConstructorExpression;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Repository;
-
 import java.time.LocalDateTime;
 import java.util.List;
-
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Repository;
 
 /**
  * 상품 목록 조회 DAO
@@ -26,145 +24,151 @@ import lombok.RequiredArgsConstructor;
 @Repository
 @RequiredArgsConstructor
 public class ProductListDao {
-  private static final int PAGE_SIZE = 10;
-  private final JPAQueryFactory queryFactory;
+    private static final int PAGE_SIZE = 10;
+    private final JPAQueryFactory queryFactory;
 
-  public List<ProductListResponse> selectAll(int page){
-    QProduct product = QProduct.product;
-    QProductSale sale = QProductSale.productSale;
-    LocalDateTime curTime = LocalDateTime.now();
+    public List<ProductListResponse> selectAll(int page) {
+        QProduct product = QProduct.product;
+        QProductSale sale = QProductSale.productSale;
+        LocalDateTime curTime = LocalDateTime.now();
 
-
-    return queryFactory.select(getProjection())
-            .from(product)
-            .leftJoin(sale)
-            .on(sale.productSeq.eq(product.productSeq),
-                    sale.saleStartAt.loe(curTime).and(sale.saleEndAt.goe(curTime)))
-            .offset(PAGE_SIZE * page)
-            .limit((PAGE_SIZE) + 1)
-            .fetch();
-  }
-
-  /**
-   * 상품 목록 조회
-   *
-   * @param param 요청 파라미터
-   * @return 상품 목록
-   */
-  public List<ProductListResponse> select(ProductListRequest param) {
-    QProduct product = QProduct.product;
-    QProductSale sale = QProductSale.productSale;
-    LocalDateTime curTime = LocalDateTime.now();
-
-    return queryFactory.select(getProjection())
-                       .from(product)
-                       .leftJoin(sale)
-                       .on(sale.productSeq.eq(product.productSeq),
-                           sale.saleStartAt.loe(curTime).and(sale.saleEndAt.goe(curTime)))
-                       .where(getCondition(param))
-                       .orderBy(getOrder(param.getSort()))
-            .offset(PAGE_SIZE * param.getPage())
-            .limit(PAGE_SIZE + 1)
-                       .fetch();
-  }
-
-  /**
-   * 상품 검색 조회
-   *
-   * @param param 요청 파라미터
-   * @return 상품 목록
-   */
-  public List<ProductListResponse> select(ProductSearchRequest param) {
-    QProduct product = QProduct.product;
-    QProductSale sale = QProductSale.productSale;
-    LocalDateTime curTime = LocalDateTime.now();
-
-    return queryFactory.select(getProjection())
-                       .from(product)
-                       .leftJoin(sale)
-                       .on(sale.productSeq.eq(product.productSeq),
-                           sale.saleStartAt.loe(curTime).and(sale.saleEndAt.goe(curTime)))
-                       .where(getCondition(param))
-                       .orderBy(getOrder(param.getSort()))
-            .offset(PAGE_SIZE * param.getPage())
-            .limit((PAGE_SIZE) + 1)
-                       .fetch();
-  }
-
-  /**
-   * 상품 목록 조회 조건 반환
-   */
-  private BooleanBuilder getCondition(ProductListRequest param) {
-    BooleanBuilder booleanBuilder = new BooleanBuilder();
-    QProduct product = QProduct.product;
-
-    // 유효여부
-    booleanBuilder.and(product.valid.eq(Boolean.TRUE));
-    // 카테고리
-    booleanBuilder.and(product.category.categorySeq.eq(param.getCategorySeq()));
-
-    // 낮은 가격
-    if (param.getLowPrice() != null) {
-      booleanBuilder.and(product.price.goe(param.getLowPrice()));
-    }
-    // 높은 가격
-    if (param.getHighPrice() != null) {
-      booleanBuilder.and(product.price.loe(param.getHighPrice()));
+        return queryFactory.select(getProjection())
+                .from(product)
+                .leftJoin(sale)
+                .on(sale.productSeq.eq(product.productSeq),
+                        sale.saleStartAt.loe(curTime).and(sale.saleEndAt.goe(curTime)))
+                .offset(PAGE_SIZE * page)
+                .limit((PAGE_SIZE) + 1)
+                .fetch();
     }
 
-    return booleanBuilder;
-  }
+    /**
+     * 상품 목록 조회
+     *
+     * @param param 요청 파라미터
+     * @return 상품 목록
+     */
+    public List<ProductListResponse> select(ProductListRequest param) {
+        QProduct product = QProduct.product;
+        QProductSale sale = QProductSale.productSale;
+        QProductLike productLike = QProductLike.productLike;
+        LocalDateTime curTime = LocalDateTime.now();
 
-  /**
-   * 상품 검색 조회 조건 반환
-   */
-  private BooleanBuilder getCondition(ProductSearchRequest param) {
-    BooleanBuilder booleanBuilder = new BooleanBuilder();
-    QProduct product = QProduct.product;
-
-    // 유효여부
-    booleanBuilder.and(product.valid.eq(Boolean.TRUE));
-    // 카테고리
-    if (param.getCategorySeq() != null) {
-      booleanBuilder.and(product.category.categorySeq.eq(param.getCategorySeq()));
+        return queryFactory.select(getProjection())
+                .from(product)
+                .leftJoin(sale)
+                .on(sale.productSeq.eq(product.productSeq),
+                        sale.saleStartAt.loe(curTime).and(sale.saleEndAt.goe(curTime)))
+                .leftJoin(productLike)
+                .on(productLike.productLikeKey.product.productSeq.eq(product.productSeq))
+                .where(getCondition(param))
+                .orderBy(getOrder(param.getSort()))
+                .offset(PAGE_SIZE * param.getPage())
+                .limit(PAGE_SIZE + 1)
+                .fetch();
     }
 
-    // 검색 키워드
-    if (StringUtils.isNotBlank(param.getKeyword())) {
-      booleanBuilder.and(product.name.like("%" + param.getKeyword() + "%"));
-    }
-    // 낮은 가격
-    if (param.getLowPrice() != null) {
-      booleanBuilder.and(product.price.goe(param.getLowPrice()));
-    }
-    // 높은 가격
-    if (param.getHighPrice() != null) {
-      booleanBuilder.and(product.price.loe(param.getHighPrice()));
+    /**
+     * 상품 검색 조회
+     *
+     * @param param 요청 파라미터
+     * @return 상품 목록
+     */
+    public List<ProductListResponse> select(ProductSearchRequest param) {
+        QProduct product = QProduct.product;
+        QProductSale sale = QProductSale.productSale;
+        QProductLike productLike = QProductLike.productLike;
+        LocalDateTime curTime = LocalDateTime.now();
+
+        return queryFactory.select(getProjection())
+                .from(product)
+                .leftJoin(sale)
+                .on(sale.productSeq.eq(product.productSeq),
+                        sale.saleStartAt.loe(curTime).and(sale.saleEndAt.goe(curTime)))
+                .leftJoin(productLike)
+                .on(productLike.productLikeKey.product.productSeq.eq(product.productSeq))
+                .where(getCondition(param))
+                .orderBy(getOrder(param.getSort()))
+                .offset(PAGE_SIZE * param.getPage())
+                .limit((PAGE_SIZE) + 1)
+                .fetch();
     }
 
-    return booleanBuilder;
-  }
+    /**
+     * 상품 목록 조회 조건 반환
+     */
+    private BooleanBuilder getCondition(ProductListRequest param) {
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QProduct product = QProduct.product;
 
-  private OrderSpecifier<?> getOrder(String sort) {
-    QProduct product = QProduct.product;
+        // 유효여부
+        booleanBuilder.and(product.valid.eq(Boolean.TRUE));
+        // 카테고리
+        booleanBuilder.and(product.category.categorySeq.eq(param.getCategorySeq()));
 
-    if (StringUtils.isBlank(sort)) {
-      return product.createdAt.desc();
+        // 낮은 가격
+        if (param.getLowPrice() != null) {
+            booleanBuilder.and(product.price.goe(param.getLowPrice()));
+        }
+        // 높은 가격
+        if (param.getHighPrice() != null) {
+            booleanBuilder.and(product.price.loe(param.getHighPrice()));
+        }
+
+        return booleanBuilder;
     }
 
-    if (StringUtils.equals(sort, ProductSortType.NEWEST.getCode())) {
-      return product.createdAt.desc();
-    } else if (StringUtils.equals(sort, ProductSortType.HIGHEST.getCode())) {
-      return product.price.desc();
-    } else {
-      return product.price.asc();
-    }
-  }
+    /**
+     * 상품 검색 조회 조건 반환
+     */
+    private BooleanBuilder getCondition(ProductSearchRequest param) {
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QProduct product = QProduct.product;
 
-  private ConstructorExpression<ProductListResponse> getProjection() {
-    QProduct product = QProduct.product;
-    QProductSale sale = QProductSale.productSale;
-    return Projections.constructor(ProductListResponse.class, product.productSeq, product.name, product.price,
-        sale.salePrice, product.brand, product.thumbnailImage);
-  }
+        // 유효여부
+        booleanBuilder.and(product.valid.eq(Boolean.TRUE));
+        // 카테고리
+        if (param.getCategorySeq() != null) {
+            booleanBuilder.and(product.category.categorySeq.eq(param.getCategorySeq()));
+        }
+
+        // 검색 키워드
+        if (StringUtils.isNotBlank(param.getKeyword())) {
+            booleanBuilder.and(product.name.like("%" + param.getKeyword() + "%"));
+        }
+        // 낮은 가격
+        if (param.getLowPrice() != null) {
+            booleanBuilder.and(product.price.goe(param.getLowPrice()));
+        }
+        // 높은 가격
+        if (param.getHighPrice() != null) {
+            booleanBuilder.and(product.price.loe(param.getHighPrice()));
+        }
+
+        return booleanBuilder;
+    }
+
+    private OrderSpecifier<?> getOrder(String sort) {
+        QProduct product = QProduct.product;
+
+        if (StringUtils.isBlank(sort)) {
+            return product.createdAt.desc();
+        }
+
+        if (StringUtils.equals(sort, ProductSortType.NEWEST.getCode())) {
+            return product.createdAt.desc();
+        } else if (StringUtils.equals(sort, ProductSortType.HIGHEST.getCode())) {
+            return product.price.desc();
+        } else {
+            return product.price.asc();
+        }
+    }
+
+    private ConstructorExpression<ProductListResponse> getProjection() {
+        QProduct product = QProduct.product;
+        QProductSale sale = QProductSale.productSale;
+        QProductLike productLike = QProductLike.productLike;
+        return Projections.constructor(ProductListResponse.class, product.productSeq, product.name, product.price,
+                sale.salePrice, product.brand, product.thumbnailImage, productLike.isNotNull());
+    }
 }
--- src/main/java/com/freshman/freshmanbackend/domain/product/response/ProductListResponse.java
+++ src/main/java/com/freshman/freshmanbackend/domain/product/response/ProductListResponse.java
@@ -1,7 +1,6 @@
 package com.freshman.freshmanbackend.domain.product.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-
 import lombok.Getter;
 
 /**
@@ -11,56 +10,59 @@ import lombok.Getter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductListResponse {
 
-  /**
-   * 상품 일련번호
-   */
-  private final Long productSeq;
-  /**
-   * 상품명
-   */
-  private final String name;
-  /**
-   * 가격
-   */
-  private final Long price;
-  /**
-   * 할인 정보
-   */
-  private Sale sale;
-  /**
-   * 브랜드명
-   */
-  private final String brand;
-  /**
-   * 목록 이미지
-   */
-  private final String image;
-
-  public ProductListResponse(Long productSeq, String name, Long price, Long salePrice, String brand, String image) {
-    this.productSeq = productSeq;
-    this.name = name;
-    this.price = price;
-    if (salePrice != null) {
-      this.sale = new Sale(salePrice, (int) (((float) (price - salePrice) / price) * 100));
-    }
-    this.brand = brand;
-    this.image = image;
-  }
-
-  @Getter
-  public static class Sale {
     /**
-     * 할인 가격
+     * 상품 일련번호
      */
-    private final Long salePrice;
+    private final Long productSeq;
     /**
-     * 할인율
+     * 상품명
      */
-    private final Integer saleRate;
+    private final String name;
+    /**
+     * 가격
+     */
+    private final Long price;
+    /**
+     * 브랜드명
+     */
+    private final String brand;
+    /**
+     * 목록 이미지
+     */
+    private final String image;
+    private final Boolean liked;
+    /**
+     * 할인 정보
+     */
+    private Sale sale;
 
-    public Sale(Long salePrice, Integer saleRate) {
-      this.salePrice = salePrice;
-      this.saleRate = saleRate;
+    public ProductListResponse(Long productSeq, String name, Long price, Long salePrice, String brand, String image,
+                               Boolean liked) {
+        this.productSeq = productSeq;
+        this.name = name;
+        this.price = price;
+        if (salePrice != null) {
+            this.sale = new Sale(salePrice, (int) (((float) (price - salePrice) / price) * 100));
+        }
+        this.brand = brand;
+        this.image = image;
+        this.liked = liked;
     }
-  }
+
+    @Getter
+    public static class Sale {
+        /**
+         * 할인 가격
+         */
+        private final Long salePrice;
+        /**
+         * 할인율
+         */
+        private final Integer saleRate;
+
+        public Sale(Long salePrice, Integer saleRate) {
+            this.salePrice = salePrice;
+            this.saleRate = saleRate;
+        }
+    }
 }
