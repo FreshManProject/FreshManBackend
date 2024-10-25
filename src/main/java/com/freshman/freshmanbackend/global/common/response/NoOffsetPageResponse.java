8ab6e900a714abd56506e4067d64d878568f6068
Chanho lee <devchlee12@gmail.com>
2024-10-26 00:42:33 +0900

    [FIX] 가격순 정렬을 위한 NoOffsetPageResponse 수정

--- src/main/java/com/freshman/freshmanbackend/global/common/response/NoOffsetPageResponse.java
+++ src/main/java/com/freshman/freshmanbackend/global/common/response/NoOffsetPageResponse.java
@@ -1,20 +1,29 @@
 package com.freshman.freshmanbackend.global.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Getter;
-
 import java.util.Collection;
+import lombok.Getter;
 
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class NoOffsetPageResponse extends SuccessResponse{
+public class NoOffsetPageResponse extends SuccessResponse {
     private final Collection<?> list;
     private final int count;
     private final Boolean isEnd;
+    private Long nextSeq;
+    private Long nextPrice;
 
     public NoOffsetPageResponse(Collection<?> list, Boolean isEnd) {
         this.list = list;
         this.isEnd = isEnd;
         this.count = list == null ? 0 : list.size();
+    }
+
+    public NoOffsetPageResponse(Collection<?> list, Boolean isEnd, Long nextSeq, Long nextPrice) {
+        this.list = list;
+        this.isEnd = isEnd;
+        this.count = list == null ? 0 : list.size();
+        this.nextSeq = nextSeq;
+        this.nextPrice = nextPrice;
     }
 }
