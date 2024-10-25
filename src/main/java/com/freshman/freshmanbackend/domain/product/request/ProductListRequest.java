d7d1cc3f60996aa68f3e42b86525fc27dbf465f7
Chanho lee <devchlee12@gmail.com>
2024-10-25 13:54:10 +0900

    [FIX] 페이지 설정 안해도 기본값 설정

--- src/main/java/com/freshman/freshmanbackend/domain/product/request/ProductListRequest.java
+++ src/main/java/com/freshman/freshmanbackend/domain/product/request/ProductListRequest.java
@@ -10,25 +10,25 @@ import lombok.Setter;
 @Setter
 public class ProductListRequest {
 
-  /**
-   * 카테고리 일련번호
-   */
-  private Long categorySeq;
-  /**
-   * 낮은 가격
-   */
-  private Integer lowPrice;
-  /**
-   * 높은 가격
-   */
-  private Integer highPrice;
-  /**
-   * 정렬
-   */
-  private String sort;
+    /**
+     * 카테고리 일련번호
+     */
+    private Long categorySeq;
+    /**
+     * 낮은 가격
+     */
+    private Integer lowPrice;
+    /**
+     * 높은 가격
+     */
+    private Integer highPrice;
+    /**
+     * 정렬
+     */
+    private String sort;
 
-  /**
-   * 페이지
-   */
-  private Integer page;
+    /**
+     * 페이지
+     */
+    private Integer page = 0;
 }
