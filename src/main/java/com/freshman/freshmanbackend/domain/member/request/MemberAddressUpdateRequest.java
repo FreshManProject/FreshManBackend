8859665beb369117b323683f3b00119db928f9c0
Chanho lee <devchlee12@gmail.com>
2024-11-13 18:46:50 +0900

    [TEST] Request 객체 빌더 추가

--- src/main/java/com/freshman/freshmanbackend/domain/member/request/MemberAddressUpdateRequest.java
+++ src/main/java/com/freshman/freshmanbackend/domain/member/request/MemberAddressUpdateRequest.java
@@ -1,5 +1,8 @@
 package com.freshman.freshmanbackend.domain.member.request;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +10,8 @@ import lombok.NoArgsConstructor;
  * 멤버 주소 업데이트 요청
  */
 @NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 @Getter
 public class MemberAddressUpdateRequest {
     private String address;
--- src/main/java/com/freshman/freshmanbackend/domain/member/request/MemberInfoUpdateRequest.java
+++ src/main/java/com/freshman/freshmanbackend/domain/member/request/MemberInfoUpdateRequest.java
@@ -1,5 +1,8 @@
 package com.freshman.freshmanbackend.domain.member.request;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,9 +10,11 @@ import lombok.NoArgsConstructor;
  * 멤버 개인정보 수정 요청
  */
 @Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
 public class MemberInfoUpdateRequest {
-        private String name;
-        private String email;
-        private String phone;
+    private String name;
+    private String email;
+    private String phone;
 }
